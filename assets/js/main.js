2332fea8971a21a2bc1defb75b14ca2ae78561dd
Alex <88356153+Alexxmfs@users.noreply.github.com>
2023-08-18 19:06:56 -0300

    bulding portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,7 +90,19 @@ const modalViews = document.querySelectorAll('.services__modal'),
         })
     })
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
 
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
